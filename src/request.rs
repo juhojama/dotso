0f613058939c84b7ffc0868b374fd93c1ce5b76f
Juho Jama <juho.jama@gmail.com>
2023-11-18 14:45:32 +0200

    Add request

---
+++ src/request.rs
@@ -0,0 +1,31 @@
+use crate::{errors::HttpClientError, response::RESPONSE_CLASS};
+use phper::classes::{ClassEntity, StaticStateClass, Visibility};
+use reqwest::blocking::RequestBuilder;
+use std::{convert::Infallible, mem::take};
+
+pub const REQUEST_BUILDER_CLASS_NAME: &str = "HttpClient\\RequestBuilder";
+
+pub static REQUEST_BUILDER_CLASS: StaticStateClass<Option<RequestBuilder>> =
+    StaticStateClass::null();
+
+pub fn make_request_builder_class() -> ClassEntity<Option<RequestBuilder>> {
+    let mut class = ClassEntity::<Option<RequestBuilder>>::new_with_default_state_constructor(
+        REQUEST_BUILDER_CLASS_NAME,
+    );
+
+    class.bind(&REQUEST_BUILDER_CLASS);
+
+    class.add_method("__construct", Visibility::Private, |_, _| {
+        Ok::<_, Infallible>(())
+    });
+
+    class.add_method("send", Visibility::Public, |this, _arguments| {
+        let state = take(this.as_mut_state());
+        let response = state.unwrap().send().map_err(HttpClientError::Reqwest)?;
+        let mut object = RESPONSE_CLASS.new_object([])?;
+        *object.as_mut_state() = Some(response);
+        Ok::<_, phper::Error>(object)
+    });
+
+    class
+}
