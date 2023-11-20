025736eb63c388476b398d0a9b00835470a3f483
Juho Jama <juho.jama@gmail.com>
2023-11-20 15:58:36 +0200

    Add response

---
+++ src/response.rs
@@ -0,0 +1,59 @@
+use crate::errors::HttpClientError;
+use phper::{
+    arrays::{InsertKey, ZArray},
+    classes::{ClassEntity, StaticStateClass, Visibility},
+    values::ZVal,
+};
+use reqwest::blocking::Response;
+use std::mem::take;
+
+pub const RESPONSE_CLASS_NAME: &str = "HttpClient\\Response";
+
+pub static RESPONSE_CLASS: StaticStateClass<Option<Response>> = StaticStateClass::null();
+
+pub fn make_response_class() -> ClassEntity<Option<Response>> {
+    let mut class =
+        ClassEntity::<Option<Response>>::new_with_default_state_constructor(RESPONSE_CLASS_NAME);
+
+    class.bind(&RESPONSE_CLASS);
+
+    class.add_method("body", Visibility::Public, |this, _arguments| {
+        let response = take(this.as_mut_state());
+        let response = response.ok_or(HttpClientError::ResponseHadRead)?;
+        let body = response.bytes().map_err(HttpClientError::Reqwest)?;
+        Ok::<_, phper::Error>(body.to_vec())
+    });
+
+    class.add_method("status", Visibility::Public, |this, _arguments| {
+        let response =
+            this.as_state()
+                .as_ref()
+                .ok_or_else(|| HttpClientError::ResponseAfterRead {
+                    method_name: "status".to_owned(),
+                })?;
+
+        Ok::<_, HttpClientError>(response.status().as_u16() as i64)
+    });
+
+    class.add_method("headers", Visibility::Public, |this, _arguments| {
+        let response =
+            this.as_state()
+                .as_ref()
+                .ok_or_else(|| HttpClientError::ResponseAfterRead {
+                    method_name: "headers".to_owned(),
+                })?;
+        let headers_map = response
+            .headers()
+            .iter()
+            .fold(ZArray::new(), |mut acc, (key, value)| {
+                let arr = acc.entry(key.as_str()).or_insert(ZVal::from(ZArray::new()));
+                arr.as_mut_z_arr()
+                    .unwrap()
+                    .insert(InsertKey::NextIndex, ZVal::from(value.as_bytes()));
+                acc
+            });
+        Ok::<_, HttpClientError>(headers_map)
+    });
+
+    class
+}
