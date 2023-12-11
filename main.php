3ca4e4c5be4f5251d66afd2e22c34f04833dea96
Juho Jama <juho.jama@gmail.com>
2023-12-11 19:01:14 +0200

    Update main

--- main.php
+++ main.php
@@ -1,19 +1,7 @@
 <?php
 
-use HttpClient\HttpClientBuilder;
-use HttpClient\HttpClientException;
+use Dotso\CouchDB;
 
-$client = (new HttpClientBuilder())->timeout(15000)->cookie_store(true)->build();
+$couchDB = new CouchDB(databaseHost: '127.0.0.1:5984');
 
-$response = $client->get('https://google.com/')->send();
-var_dump([
-    'status' => $response->status(),
-    'headers' => $response->headers(),
-]);
-
-// Prints: builder error for url (file:///): URL scheme is not allowed
-try {
-    $client->get('file:///')->send();
-} catch (HttpClientException $e) {
-    var_dump($e->getMessage());
-}
+var_dump($couchDB);
