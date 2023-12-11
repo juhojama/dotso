0e8bf90cce8338c8a4d7a2b8d215e9008cf9318c
Juho Jama <juho.jama@gmail.com>
2023-12-11 20:00:22 +0200

    Add class property

--- src/couchdb.rs
+++ src/couchdb.rs
@@ -9,8 +9,12 @@ const COUCHDB_CLASS_NAME: &str = "Dotso\\CouchDB";
 pub fn make_couchdb_class() -> ClassEntity<()> {
     let mut class = ClassEntity::new(COUCHDB_CLASS_NAME);
 
+    class.add_property("url", Visibility::Public, ());
+
     class
-        .add_method("__construct", Visibility::Public, |_, _| {
+        .add_method("__construct", Visibility::Public, move |this, arguments| {
+            let url = arguments[0].expect_z_str().unwrap().to_str().unwrap();
+            this.set_property("url", url);
             Ok::<_, Infallible>(())
         })
         .argument(Argument::by_val("databaseHost"));
