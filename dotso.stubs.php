dc7c53a25c8371fd7a64208e4ae67d4d62d27820
Juho Jama <juho.jama@gmail.com>
2023-12-11 19:01:24 +0200

    Update stubs

---
+++ dotso.stubs.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Stubs for documentation.
+ */
+
+namespace Dotso;
+
+class Response
+{
+    function status()
+    {
+    }
+
+    function headers()
+    {
+    }
+
+    function body()
+    {
+    }
+}
+
+class CouchDB
+{
+    public function __construct(string $databaseHost)
+    {
+    }
+}
