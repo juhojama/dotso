07c3a2fe01a48230c064b6df7c5c8b4033190397
Juho Jama <juho.jama@gmail.com>
2023-12-17 20:07:00 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,13 @@
 <?php
 
 use Dotso\CouchDB;
+use Dotso\HttpClientBuilder;
 
-$couchDB = new CouchDB(databaseHost: '127.0.0.1:5984');
+$client = (new HttpClientBuilder())
+    ->timeout(15000)
+    ->cookie_store(true)
+    ->build();
 
-var_dump($couchDB);
+$couchDB = new CouchDB(url: 'http://127.0.0.1:5984', client: $client);
+$result = $couchDB->get(database: 'users', id: '123');
+var_dump($result);
